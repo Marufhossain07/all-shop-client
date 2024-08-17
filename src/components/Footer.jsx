c6676f04b2fe180dc897aa16db50f8bfb43c66ba
Marufhossain07 <marufprogrammer07@gmail.com>
2024-08-17 18:06:28 +0600

    footer added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,6 @@ const Footer = () => {
             <div className="container flex flex-col justify-around  py-10 mx-auto space-y-8 lg:flex-row lg:space-y-0">
                 <div>
                     <a className="btn btn-ghost text-4xl font-sedan lg:pl-0 text-white">AllShop</a>
-
                 </div>
                 <div className="grid gap-5 md:grid-cols-2 lg:grid-cols-2  w-1/2 ">
                     <div className="space-y-3">
@@ -17,7 +16,6 @@ const Footer = () => {
                                     <path d="M32 16c0-8.839-7.167-16-16-16-8.839 0-16 7.161-16 16 0 7.984 5.849 14.604 13.5 15.803v-11.177h-4.063v-4.625h4.063v-3.527c0-4.009 2.385-6.223 6.041-6.223 1.751 0 3.584 0.312 3.584 0.312v3.937h-2.021c-1.984 0-2.604 1.235-2.604 2.5v3h4.437l-0.713 4.625h-3.724v11.177c7.645-1.199 13.5-7.819 13.5-15.803z"></path>
                                 </svg>
                             </a>
-
                         </div>
                     </div>
                     <form>
@@ -34,7 +32,7 @@ const Footer = () => {
                     </form>
                 </div>
             </div>
-            <div className="py-6 text-sm text-center dark:text-gray-600">©2025 AllShop. All rights reserved.</div>
+            <div className="py-6 text-sm text-center dark:text-gray-600">©2026 AllShop. All rights reserved.</div>
         </footer>
     );
 };
