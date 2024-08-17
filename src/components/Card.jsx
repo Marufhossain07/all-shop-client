c5eefef3a71b9a1ae17ba13915c693a346aed9a3
Marufhossain07 <marufprogrammer07@gmail.com>
2024-08-18 02:35:08 +0600

    card added and responsive done

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -12,11 +12,15 @@ const Card = ({product}) => {
                     className="rounded-xl" />
             </figure>
             <div className="card-body pt-0 text-white">
+                <div className=' flex  items-center justify-between'>
+                    <p>Date: {product?.createdAt.slice(0,10)}</p>
+                    <p className='font-semibold grow-0 text-lg'>Rating: {product.ratings}</p>
+                </div>
                 <p className='font-medium'>Brand: {product?.brand}</p>
                 <h2 className="card-title font-semibold text-2xl">{product?.name}</h2>
                 <p>{product?.description}</p>
                 <div className="card-actions items-center">
-                    <p className='font-medium'>Price: {product?.price}</p>
+                    <p className='font-medium text-lg'>Price: {product?.price}</p>
                     <button className="btn bg-white text-blue-400">Buy Now</button>
                 </div>
             </div>
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,25 +1,98 @@
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from '../components/Card';
+import { FaSearch } from 'react-icons/fa';
 
 const Products = () => {
     const [search, setSearch] = useState('');
     const [searchText, setSearchText] = useState('');
     const [price, setPrice] = useState('')
+    const [count, setCount] = useState(0)
+    const [max, setMax] = useState(2000)
+    const [productPerPage, setProductPerPage] = useState(12)
+    const [currentPage, setCurrentPage] = useState(1)
     const [date, setDate] = useState('')
     const [brand, setBrand] = useState('')
     const [category, setCategory] = useState('')
 
     const { data, isLoading } = useQuery({
-        queryKey: ['product'],
+        queryKey: ['product', category, brand, date, price, currentPage, search,max],
         queryFn: async () => {
-            const { data } = await axios.get('http://localhost:5000/products')
+            const { data } = await axios.get(`http://localhost:5000/products?search=${search}&brand=${brand}&category=${category}&price=${price}&date=${date}&page=${currentPage}&size=${productPerPage}&max=${max}`)
             return data
         }
     });
     console.log(data);
 
+
+
+    useEffect(() => {
+        const getCount = async () => {
+            const { data } = await axios.get('http://localhost:5000/products-count')
+            setCount(data.result)
+        }
+        getCount()
+    }, [axios])
+
+    const brands = [
+        "SoundWave",
+        "TechPro",
+        "RunEase",
+        "CaptureMax",
+        "QuickBoil",
+        "GameMaster",
+        "TimeLux",
+        "QuietZone",
+        "FitPulse",
+        "VisionPro",
+        "ChargeMate",
+        "SmilePro",
+        "EcoTemp",
+        "GlideRide",
+        "BrightLite",
+        "TypeEase",
+        "PowerMax",
+        "AdventureCam",
+        "SleepWave",
+        "ErgoLift",
+        "SecureHome",
+        "CleanBot",
+        "AudioPulse",
+        "CookMaster",
+        "ShavePro",
+        "VirtualEye",
+        "HydroSmart",
+        "WarmCozy",
+        "BlendGo",
+        "BrewMaster",
+        "FitBalance",
+        "ReflectFit",
+        "PowerControl",
+        "EcoRide",
+        "CoolBreeze",
+        "PowerWash",
+        "CinemaGo",
+        "TechTime",
+        "AutoDrape",
+        "CampChef"
+    ]
+
+    const categories = [
+        "Electronics",
+        "Footwear",
+        "Home Appliances",
+        "Accessories",
+        "Wearables",
+        "Personal Care",
+        "Home Automation",
+        "Outdoor"
+    ]
+
+
+    const handlePagination = (value) => {
+        setCurrentPage(value);
+    }
 
     const handlePrice = (e) => {
         e.preventDefault()
@@ -44,54 +117,122 @@ const Products = () => {
         e.preventDefault()
         setSearch(searchText)
     }
-    console.log(searchText);
+
+
+    const totalProducts = Math.ceil(count / productPerPage)
+    const pages = [...Array(totalProducts).keys()].map(element => element + 1)
 
     return (
         <div className='container mx-auto'>
             <h3 className='text-3xl mb-10 font-medium text-blue-400 text-center'>Products</h3>
-            <div className='flex flex-col  lg:flex-row justify-center items-center  gap-5'>
-                <div className='flex flex-wrap gap-5'>
-                    <select onChange={handleBrand} className="select select-info border-blue-400 ">
+            <div className='flex flex-col   lg:flex-row justify-center items-center  gap-5'>
+                <div className='w-full lg:w-1/4'>
+                <div className='flex justify-between items-center'>
+                <h3 className='font-semibold text-center mb-2'>Filter By Price Range</h3>
+                <p className='font-medium'>Max Price : {max}</p>
+                </div>
+                    <input onChange={(e)=> setMax(e.target.value)} value={max} type="range" min={0} max="2000"  className="range range-info" step="500" />
+                </div>
+                <div className='flex w-full lg:w-auto gap-5'>
+                    <select onChange={handleBrand} className="select w-full  lg:w-auto select-info border-blue-400 ">
                         <option disabled selected>Filter By Brand</option>
-                        <option value={'bata'}>Bata</option>
-                        <option value={'nike'}>Nike</option>
+                        {brands.map(b => <option value={b}>{b}</option>)}
                     </select>
-                    <select onChange={handleCategory} className="select select-info  border-blue-400 ">
+                    <select onChange={handleCategory} className="select w-full lg:w-auto  select-info  border-blue-400 ">
                         <option disabled selected>Filter By Category</option>
-                        <option value={'shoe'}>Shoe</option>
-                        <option value={'shirt'}>Shirt</option>
+                        {categories.map(c => <option value={c}>{c}</option>)}
                     </select>
                 </div>
-                <form onSubmit={handleSearch} className=''>
-                    <label className="input    mx-auto border border-blue-400 flex items-center gap-2">
+                <form onSubmit={handleSearch} className='w-full lg:w-auto '>
+                    <label className="input  mx-auto border border-blue-400 flex items-center gap-2">
                         <input onChange={e => setSearchText(e.target.value)} name="search" type="text" value={searchText} className="grow" placeholder="Search" />
-                        <button><p className="text-2xl text-[#669bbc]">Search</p></button>
-                        {/* <button><FaSearch className="text-2xl text-[#669bbc]" /></button> */}
+                        <button><FaSearch className="text-2xl text-blue-400" /></button>
                     </label>
                 </form>
-                <div className='flex flex-wrap gap-5'>
-                    <select onChange={handlePrice} className="select select-info  border-blue-400 ">
+                <div className='flex flex-wrap w-full lg:w-auto gap-5'>
+                    <select onChange={handlePrice} className="select select-info w-full lg:w-auto border-blue-400 ">
                         <option disabled selected>Sort By Price</option>
                         <option value={'low'}>Low to High</option>
                         <option value={'high'}>High to Low</option>
                     </select>
-                    <select onChange={handleDate} className="select select-info  border-blue-400">
+                    <select onChange={handleDate} className="select select-info w-full lg:w-auto  border-blue-400">
                         <option disabled selected>Sort By Date</option>
                         <option value={'new'}>Newest Fast</option>
                     </select>
                 </div>
+                <button onClick={() => { setBrand(''), setCategory(''), setSearch(''), setDate(''), setSearchText(''), setPrice(''), setMax(2000) }} className="btn bg-blue-400 text-white">Reset</button>
             </div>
 
             {
                 isLoading ? <span className="loading  ml-[50%] mr-[50%] loading-lg mt-48 loading-spinner text-info"></span> : <div className='grid lg:grid-cols-3 my-10 gap-5'>
                     {
-                        data?.map((product)=>{
-                           return <Card product={product}></Card>
+                        data?.map((product) => {
+                            return <Card product={product}></Card>
                         })
                     }
                 </div>
-            }
 
+            }
+            <div className='flex justify-center my-12'>
+                <button
+                    disabled={currentPage === 1}
+                    onClick={() => handlePagination(currentPage - 1)}
+                    className='px-4 py-2 mx-1 bg-blue-400 text-white capitalize rounded-md disabled:cursor-not-allowed disabled:hover:bg-gray-200 disabled:hover:text-gray-500 hover:bg-blue-400  hover:text-white'
+                >
+                    <div className='flex items-center -mx-1'>
+                        <svg
+                            xmlns='http://www.w3.org/2000/svg'
+                            className='w-6 h-6 mx-1 rtl:-scale-x-100'
+                            fill='none'
+                            viewBox='0 0 24 24'
+                            stroke='currentColor'
+                        >
+                            <path
+                                strokeLinecap='round'
+                                strokeLinejoin='round'
+                                strokeWidth='2'
+                                d='M7 16l-4-4m0 0l4-4m-4 4h18'
+                            />
+                        </svg>
+
+                        <span className='mx-1'>Prev</span>
+                    </div>
+                </button>
+                {pages.map(page => (
+                    <button
+                        onClick={() => handlePagination(page)}
+                        key={page}
+                        className={`hidden ${currentPage === page ? 'bg-blue-400 text-white' : ''
+                            } px-4 py-2 mx-1 transition-colors duration-300 transform  rounded-md sm:inline hover:bg-blue-400  hover:text-white`}
+                    >
+                        {page}
+                    </button>
+                ))}
+                <button
+                    disabled={currentPage === totalProducts}
+                    onClick={() => handlePagination(currentPage + 1)}
+                    className='px-4 py-2 mx-1 bg-blue-400 text-white transition-colors duration-300 transform  rounded-md hover:bg-black  disabled:hover:bg-gray-200 disabled:hover:text-gray-500 hover:text-white disabled:cursor-not-allowed disabled:text-gray-500'
+                >
+                    <div className='flex items-center -mx-1'>
+                        <span className='mx-1'>Next</span>
+
+                        <svg
+                            xmlns='http://www.w3.org/2000/svg'
+                            className='w-6 h-6 mx-1 rtl:-scale-x-100'
+                            fill='none'
+                            viewBox='0 0 24 24'
+                            stroke='currentColor'
+                        >
+                            <path
+                                strokeLinecap='round'
+                                strokeLinejoin='round'
+                                strokeWidth='2'
+                                d='M17 8l4 4m0 0l-4 4m4-4H3'
+                            />
+                        </svg>
+                    </div>
+                </button>
+            </div>
         </div>
     );
 };
